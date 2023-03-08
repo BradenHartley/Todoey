8b1057a336b4341b098e599ede4f8ef7c685e9c7
Braden Hartley <Bradenhartley@hotmail.com>
2023-03-07 20:56:35 -0700

    first commit

---
+++ Todoey/Controllers/CategoryViewController.swift
@@ -0,0 +1,39 @@
+//
+//  TableViewController.swift
+//  Todoey
+//
+//  Created by Braden Hartley on 7/18/22.
+//  Copyright © 2022 App Brewery. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+
+    var categories = [Category]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    //MARK: - TableView DataSource Methods
+    
+    
+    //MARK: - Data Manipulation Methods
+
+    
+    //MARK: - Add New Catagories
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+    }
+    
+   
+    
+    
+    //MARK: - TableView Delegate Methods
+    
+    
+   
+}
